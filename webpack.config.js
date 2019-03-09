3120f495a3105147d5da3a44c22f57f97962e992
Andre Mategka <mategka.andre@gmail.com>
2019-03-09 20:23:07 +0100

    Apply workarounds for errors in output declaration files

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const CleanPlugin = require('clean-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const TSDeclarationPlugin = require('ts-declaration-webpack-plugin');
 const RemoveFilesPlugin = require('remove-files-webpack-plugin');
+const ReplaceInFilePlugin = require('replace-in-file-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -48,6 +49,25 @@ module.exports = {
         exclude: ['dist/index.d.ts']
       }
     }),
+    // Apply workarounds for mistakes in declaration file output
+    new ReplaceInFilePlugin([
+      {
+        dir: 'dist',
+        files: ['index.d.ts'],
+        rules: [
+          // Workaround for @types dependency <reference> directives automatically being added to declaration files
+          {
+            search: /^\/\/\/.+\s+^/gm,
+            replace: ''
+          },
+          // Workaround for default imports being output as named imports in declaration files
+          {
+            search: /(?<=import ){ (\w+),/g,
+            replace: '$1, {'
+          }
+        ]
+      }
+    ])
   ],
   optimization: {
     minimizer: [
